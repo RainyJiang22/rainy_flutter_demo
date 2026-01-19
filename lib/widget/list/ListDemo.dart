4a9e9a2aaf35c65c9ada0ed6b68fdbee09c2c143
jiangshiyu <3434481891@qq.com>
2026-01-19 17:57:03 +0800

    feat(listview):列表动态刷新

--- lib/widget/list/ListDemo.dart
+++ lib/widget/list/ListDemo.dart
@@ -18,6 +18,16 @@ class ListDemoPage extends StatefulWidget {
 }
 
 class _ListDemoPageState extends State<ListDemoPage> {
+  List<Widget> widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 100; i++) {
+      widgets.add(getRow(i));
+    }
+  }
+
   List<Widget> _getListData() {
     List<Widget> widgets = [];
     for (int i = 0; i < 100; i++) {
@@ -40,7 +50,24 @@ class _ListDemoPageState extends State<ListDemoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('List Demo')),
-      body: ListView(children: _getListData()),
+      body: ListView.builder(
+        itemCount: widgets.length,
+        itemBuilder: (context, position) {
+          return getRow(position);
+        },
+      ),
+    );
+  }
+
+  Widget getRow(int i) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          widgets.add(getRow(widgets.length));
+          developer.log('row tapped $i');
+        });
+      },
+      child: Padding(padding: const EdgeInsets.all(10), child: Text('Row $i')),
     );
   }
 }
